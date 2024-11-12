086ede53c7f98e0b33c4925b8703e7096bf14180
Lekha <kachoria@wisc.edu>
2024-11-12 15:42:42 -0600

    entered in data

---
+++ app.js
@@ -0,0 +1,53 @@
+let realMadrid = {
+  name: "Real Madrid",
+  city: "Madrid",
+  country: "Spain",
+  topScorers: ["Ronaldo", "Benzema", "Hazard"],
+  worldwideFans: 798,
+};
+
+let barcelona = {
+  name: "Barcelona",
+  city: "Barcelona",
+  country: "Spain",
+  topScorers: ["Messi", "Suarez", "Puyol"],
+  worldwideFans: 738,
+};
+
+let manUnited = {
+  name: "Manchester United",
+  city: "Manchester",
+  country: "England",
+  topScorers: ["Cantona", "Rooney", "Ronaldo"],
+  worldwideFans: 755,
+};
+
+let manCity = {
+  name: "Manchester City",
+  city: "Manchester",
+  country: "England",
+  topScorers: ["Sterling", "Aguero", "Haaland"],
+  worldwideFans: 537,
+};
+
+let brazil = {
+  name: "Brazil National Team",
+  country: "Brazil",
+  topScorers: ["Ronaldinho", "Cafu", "Bebeto"],
+  worldwideFans: 950,
+};
+
+let argentina = {
+  name: "Argentina National Team",
+  country: "Argentina",
+  topScorers: ["Messi", "Batistuta", "Maradona"],
+  worldwideFans: 888,
+};
+
+let atletico = {
+  name: "Atletico Madrid",
+  city: "Madrid",
+  country: "Spain",
+  topScorers: ["Aragones", "Griezmann", "Torez"],
+  worldwideFans: 400,
+};
